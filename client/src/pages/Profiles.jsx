bda5a3421ac62d483628d2cd69adfb812ca8a633
evoreign <edbert_khovey@yahoo.com>
2023-03-10 20:30:14 +0700

    On branch main Changes to be committed: modified:   client/src/pages/Profiles.jsx

--- client/src/pages/Profiles.jsx
+++ client/src/pages/Profiles.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Profiles = () => {
   return (
-    <div>
+    <div className='relative w-full group max-w-md min-w-0 mx-auto mt-6 mb-6 
+    break-words bg-white dark:bg-gray-200 md:max-w-sm rounded-xl'>
         <h1>Profiles</h1>
         {/* create a circle of profile images */}
         <img src="https://res.cloudinary.com/dfqziqcfa/image/upload/v1678270632/cld-sample.jpg" alt="Profile image" className="w-40 h-40 rounded-full mr-4" />
